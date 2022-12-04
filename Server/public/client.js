fcf4893b53653bc955e0839c9ea819f5dd645c93
Chris Maki <c.william.developer@gmail.com>
2022-12-03 22:54:43 -0600

    Added a drawer and added photos for missing link

--- Server/public/client.js
+++ Server/public/client.js
@@ -1,2 +1,25 @@
 console.log('in client.js!!!!!!!!!!');
 
+$(document).ready(onReady);
+
+
+function onReady() {
+    
+    $(function() {
+        var open = false;
+        $('#footerSlideButton').click(function() {
+            if(open === false) {
+                $('#footerSlideContent').animate({ height: '300px' });
+                $(this).css('backgroundPosition', 'bottom left');
+                open = true;
+            } else {
+                $('#footerSlideContent').animate({ height: '0px' });
+                $(this).css('backgroundPosition', 'top left');
+                open = false;
+            } 
+        });		
+    });
+
+   
+}
+

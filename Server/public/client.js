50999065612ff0c00bb9fbc03b336c3cfd4b6341
Chris Maki <c.william.developer@gmail.com>
2022-12-06 20:10:50 -0600

    h

---
+++ Server/public/client.js
@@ -0,0 +1,96 @@
+console.log('in client.js!!!!!!!!!!');
+
+$(document).ready(onReady);
+
+
+function onReady() {
+    $('#moreAboutML').css({ display:'none' });
+    $('#moreAboutML1').css({ display:'none' });
+
+    $(function() {
+        var open = false;
+        $('#footerSlideButton').click(function() {
+            if(open === false) {
+                $('#footerSlideContent').animate({ height: '300px' });
+                $(this).css('backgroundPosition', 'bottom left');
+               
+                open = true;
+                $('#moreAboutML').css({ display:'block' });
+            } else {
+                $('#footerSlideContent').animate({ height: '0px' });
+                $(this).css('backgroundPosition', 'top left');
+               
+
+                open = false;
+                $('#moreAboutML').css({ display:'none' });
+            } 
+        });		
+    });
+
+    $(function() {
+        
+        var open = false;
+        $('#footerSlideButton1').click(function() {
+            if(open === false) {
+                $('#footerSlideContent1').animate({ height: '200px' });
+                $(this).css('backgroundPosition', 'bottom left');
+                open = true;
+                $('#moreAboutML1').css({ display:'block' });
+
+            } else {
+                $('#footerSlideContent1').animate({ height: '0px' });
+                $(this).css('backgroundPosition', 'top left');
+                open = false;
+                $('#moreAboutML1').css({ display:'none' });
+
+            } 
+        });		
+    });
+
+
+    $(function() {
+        
+        var open = false;
+        $('#footerSlideButton11').click(function() {
+            if(open === false) {
+                $('#footerSlideContent11').animate({ height: '200px' });
+                $(this).css('backgroundPosition', 'bottom left');
+                open = true;
+                $('#moreAboutML11').css({ display:'block' });
+
+            } else {
+                $('#footerSlideContent11').animate({ height: '0px' });
+                $(this).css('backgroundPosition', 'top left');
+                open = false;
+                $('#moreAboutML11').css({ display:'none' });
+
+            } 
+        });		
+    });
+   
+
+    $(window).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+      
+        $('.header-overlay').css({
+          opacity: function() {
+            var elementHeight = $(this).height();
+            return .4 - (scrollTop -elementHeight  ) / elementHeight;
+          }
+        });
+      });
+
+      $(window).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+      
+        $('.header-overlay2').css({
+          opacity: function() {
+            var elementHeight = $(this).height();
+            return 1 + (scrollTop -elementHeight  ) / elementHeight;
+          }
+        });
+      });
+
+      
+}
+
